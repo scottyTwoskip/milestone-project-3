4bdbd69742724a6dbe89a9c6048291761541e134
JDiaz <jdiaz_007@outlook.com>
2024-01-21 15:05:36 -0800

    removed tracker from dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import MyCalendar from "./MyCalendar";
-import Tracker from "./Tracker";
 
 function Dashboard() {
   return (
     <>
-        <section>
-            <Tracker />
-        </section>
         <MyCalendar />
     </>
   );
