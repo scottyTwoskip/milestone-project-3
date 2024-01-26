92b82a1cb66713e9cab79e0ddf8cf3bedcfd2412
JDiaz <jdiaz_007@outlook.com>
2024-01-25 20:37:25 -0800

    added background image to signup page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import backgroundImage from '../assets/lifting.jpg'; // Import the image
+
 function SignUp() {
   const handleSignUp = () => {
     // Your sign-up logic goes here
     console.log('Sign up button clicked');
   };
 
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '100vh', // Optional: Set a minimum height for the background
+  };
+
   return (
-    <div className="container mt-5 text-light">
+    <div className="container mt-5 text-light" style={backgroundStyle}>
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="card bg-dark">
