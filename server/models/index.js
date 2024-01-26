1d7af825478c4467ab8bc7424c4c1c7f35117cf5
JDiaz <jdiaz_007@outlook.com>
2024-01-25 18:51:14 -0800

    moved mongoose variable to index.js file in the server

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
