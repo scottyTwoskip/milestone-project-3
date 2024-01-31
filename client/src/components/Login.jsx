3de5b363b795c3d8aa70c9fb43fc2e3c966aa995
JDiaz <jdiaz_007@outlook.com>
2024-01-30 21:06:47 -0800

    added img tag in about us page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import backgroundImage from '../assets/tension_cord.jpg'; // Import the image
+import backgroundImage from '../assets/tension_cord.jpg';
 
 function Login() {
   const handleLogin = () => {
