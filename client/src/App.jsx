cb56145e47faeaf885e8d4e387f42412de763710
JDiaz <jdiaz_007@outlook.com>
2024-01-30 18:16:57 -0800

    importing navbar and nav from bootstrap to have a responsive drop down menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './components/Nav';
+import Navigation from './components/Navigation';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import AboutPage from './components/AboutPage';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+
 
 function App() {
   const [isLoggedIn, setLoggedIn] = useState(false);
@@ -28,12 +29,12 @@ function App() {
   return (
     <Router>
       <div style={pageStyle}>
-        <Nav handleLogin={handleLogin} isLoggedIn={isLoggedIn} />        
+        <Navigation handleLogin={handleLogin} isLoggedIn={isLoggedIn} /> {/* Updated usage */}
         <Routes>
           <Route path="/" element={isLoggedIn ? <Dashboard /> : <Home />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} /> 
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
