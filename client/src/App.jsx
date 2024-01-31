8db942f5d2b091d37494d955341eb718747be228
JDiaz <jdiaz_007@outlook.com>
2024-01-30 19:14:31 -0800

    updated handleLogin prop to handleAuth in navigation.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,8 +13,8 @@ function App() {
   const [isLoggedIn, setLoggedIn] = useState(false);
 
   // ! Function to handle login
-  const handleLogin = () => {
-    // Login logic goes here
+  const handleAuthentication = () => {
+    // Login / Logout logic goes here
     // Make an API call to validate credentials
 
     // If the login is successful, setLoggedIn will be (true)
@@ -29,7 +29,7 @@ function App() {
   return (
     <Router>
       <div style={pageStyle}>
-        <Navigation handleLogin={handleLogin} isLoggedIn={isLoggedIn} />
+      <Navigation handleAuth={handleAuthentication} isLoggedIn={isLoggedIn} />
         <Routes>
           <Route path="/" element={isLoggedIn ? <Dashboard /> : <Home />} />
           <Route path="/about" element={<AboutPage />} />
