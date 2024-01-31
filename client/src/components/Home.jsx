75874794672f4ccd7257aa60e0a65caf9e35f7bf
JDiaz <jdiaz_007@outlook.com>
2024-01-30 21:57:22 -0800

    added images to homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,45 +7,45 @@ function Home() {
       {/* Section 1 */}
       <div className="my-4 text-center">
         <img
-          src="path_to_image"
-          alt="Image 1"
+          src="../assets/red_black.jpg"
+          alt="Red rubber floor with black weights image by Alora Griffiths on Unsplash"
           className="img-fluid"
         />
         <h2>Ad 1</h2>
-        <p>Images or stuff can go here</p>
+        <p>TEXT GOES HERE</p>
       </div>
 
       {/* Section 2 */}
       <div className="my-4 text-center">
         <img
-          src="path_to_image"
-          alt="Image 2"
+          src="../assets/medball.jpg"
+          alt="Medicine ball and weight on block image by Ryan De Hamer on Unsplash"
           className="img-fluid"
         />
         <h2>Ad 2</h2>
-        <p>Images or stuff can go here</p>
+        <p>TEXT GOES HERE</p>
       </div>
 
       {/* Section 3 */}
       <div className="my-4 text-center">
         <img
-          src="path_to_image"
-          alt="Image 3"
+          src="../assets/ropes.jpg"
+          alt="Rope workout image by Meghan Holmes on Unsplash"
           className="img-fluid"
         />
         <h2>Ad 3</h2>
-        <p>Images or stuff can go here</p>
+        <p>TEXT GOES HERE</p>
       </div>
 
       {/* Section 4 */}
       <div className="my-4 text-center">
         <img
-          src="path_to_image"
-          alt="Image 4"
+          src="../assets/barbell.jpg"
+          alt="Barbell on ground image by Victor Freitas on Unsplash"
           className="img-fluid"
         />
         <h2>Ad 4</h2>
-        <p>Images or stuff can go here</p>
+        <p>TEXT GOES HERE</p>
       </div>
     </Container>
   );
